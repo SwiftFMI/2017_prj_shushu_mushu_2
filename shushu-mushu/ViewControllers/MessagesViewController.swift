325619d8799cc13c2ecc3a7c407b4c3c38105d2f
Pavlin Panayotov <panayotov1995@gmail.com>
2018-02-21 23:41:41 +0200

    Added MessagesViewController and UINavigationController.

---
+++ shushu-mushu/ViewControllers/MessagesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesViewController.swift
+//  shushu-mushu
+//
+//  Created by Pavlin Panayotov on 21.02.18.
+//  Copyright © 2018 Natali Arabdziyska. All rights reserved.
+//
+
+import UIKit
+
+final class MessagesViewController: ParentViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Messages"
+    }
+}
